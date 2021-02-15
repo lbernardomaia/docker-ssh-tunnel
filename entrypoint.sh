8424c0f7f22c7753c910dd79fab3bb8d1e7038dd
lbernardomaia <lbernardomaia@gmail.com>
2021-02-15 18:04:59 +0000

    Add ServerAliveInterval option

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-eval `ssh-agent -s`
-ssh-add
-while true;
 echo 'Connecting'
-do
-  ssh -o StrictHostKeyChecking=no \
-      -nNT -L *:${LOCAL_PORT}:${DESTINATION_SERVER}:${DESTINATION_PORT} \
-      $SSH_USER@$SSH_BASTION_HOST;
-  echo 'Reconnecting'
-done
+ssh -o StrictHostKeyChecking=no \
+    -o ServerAliveInterval=60 \
+    -nNT -L *:${LOCAL_PORT}:${DESTINATION_SERVER}:${DESTINATION_PORT} \
+    $SSH_USER@$SSH_BASTION_HOST;
